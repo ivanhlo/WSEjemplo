a37c5deb1c31d7870dcd31e7220714ec2e07dd4e
Ivan Hernandez <ivan.hl.mail@gmail.com>
2019-08-26 10:47:16 -0500

    Para subir a GitHub

--- ClienteWS/Properties/Settings.Designer.cs
+++ ClienteWS/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ClienteWS.Properties
-{
-
-
+namespace ClienteWS.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MJ021KNV;Initial Catalog=BD_BIBLIOCOMICS;Persist Security Info=True;U" +
+            "ser ID=ihernandezl")]
+        public string BD_BIBLIOCOMICSConnectionString {
+            get {
+                return ((string)(this["BD_BIBLIOCOMICSConnectionString"]));
             }
         }
     }
